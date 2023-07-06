943a966785dd869af7eb9cb360be4079bdb234ea
Daniel Gruno <humbedooh@apache.org>
2023-07-06 09:08:45 +0200

    Add some text to the front page

--- static/plugins/home.js
+++ static/plugins/home.js
@@ -2,7 +2,9 @@ async function render_home() {
     const page_title = document.getElementById('page_title');
     page_title.innerText = "ASF Infrastructure Reporting Dashboard";
     const page_description = document.getElementById('page_description');
-    page_description.innerText = "Some intro about our stats here....and so on";
+    page_description.innerText = "This site contains a collection of reports on the overall health and activity of the infrastructure at the ASF. \
+    Some reports are open to the public, while others are restricted to those that genuinely need them. If you cannot access a report, chances are \
+    you are not supposed to. Use the menu on the left hand side to pick a report to display.";
 
     const chart_area = document.getElementById('chart_area');
     chart_area.innerText = '';
