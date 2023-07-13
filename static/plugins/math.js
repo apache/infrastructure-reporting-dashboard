d224931e2d19ce46543cfde9b725697f7f78a6af
Daniel Gruno <humbedooh@apache.org>
2023-07-13 16:04:01 +0200

    Add Math.median and Math.sum support

---
+++ static/plugins/math.js
@@ -0,0 +1,14 @@
+// Math functions not supplied by JS
+
+Math.median = function (numbers) {
+    const numbers_sorted = Array.from(numbers).sort((a, b) => a - b);
+    const middle_pos = Math.floor(numbers_sorted.length / 2);
+    // Even number of elements, return the average of the two in the middle
+    if (numbers_sorted.length % 2 === 0) {
+        return (numbers_sorted[middle_pos - 1] + numbers_sorted[middle_pos]) / 2;
+    }
+    // Odd number of elements, return the middle one
+    return numbers_sorted[middle_pos];
+}
+
+Math.sum = (numbers) => numbers.reduce((a,b) => a+b);
