309dd8f132a5e0533d2a0cbf991909433553a034
Sebb <sebb@apache.org>
2023-09-23 23:50:42 +0100

    Filter for extensions
    Drop useless match

--- server/app/endpoints/downloads.py
+++ server/app/endpoints/downloads.py
@@ -90,9 +90,9 @@ async def process(form_data):
             q = elasticsearch_dsl.Search(using=es_client)
             q = q.filter("range", **{field_names["timestamp"]: {"gte": f"now-{duration}d"}})
             q = q.filter("match", **{field_names["request_method"]: "GET"})
-            q = q.filter("range", bytes={"gt": 5000})
-            q = q.filter("match", **{field_names["uri"]: project})
+            q = q.filter("range", bytes={"gt": 5000}) # This eliminates hashes and (most?) sigs
             q = q.filter("prefix", **{field_names["uri"] + ".keyword": f"/{project}/"})
+            q = q.filter("regexp", **{field_names['uri'] + ".keyword": r".*\.[a-z0-9]+"}) # all artifacts have an extension
             q = q.filter("match", **{field_names["vhost"]: field_names["_vhost_"]})
 
             q.aggs.bucket(
