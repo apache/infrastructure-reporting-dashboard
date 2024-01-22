8b9e043b431aaf177536b2d9d7311a3a45431b0e
Daniel Gruno <humbedooh@apache.org>
2024-01-22 16:23:00 +0100

    NR: If NP breaks, log it but continue..

--- server/app/plugins/uptime.py
+++ server/app/plugins/uptime.py
@@ -21,6 +21,7 @@ import asyncio
 from ..lib import config
 from .. import plugins
 import aiohttp
+import aiohttp.client_exceptions
 import datetime
 import dateutil.relativedelta
 import functools
@@ -40,53 +41,56 @@ async def uptime_scan(months=DEFAULT_TIMESPAN_MONTHS):
     now = datetime.datetime.now()
     cutoff_date = (now - dateutil.relativedelta.relativedelta(months=months)).strftime("%Y-%m")
     async with aiohttp.ClientSession() as hc:
-        async with hc.get(nodeping_summary_url) as req:
-            if req.status == 200:
-                hosts_json = await req.json()
-                tmpstats = {}
-                for host_entry in hosts_json.values():
-                    uuid = host_entry["uuid"]
-                    host_stats = {
-                        "uuid": uuid,
-                        "label": host_entry["label"],
-                        "uptime_monthly": {},
-                        "uptime_average": 100.0,
-                        "uptime_past_week": 100.0,
-                    }
-                    # Monthly stats per host
-                    host_url = config.reporting.uptime["host_url"].format(uuid=uuid)
-                    async with hc.get(host_url, params={"format": "json"}) as hreq:
-                        if req.status == 200:
-                            host_data = await hreq.json()
-                            uptimes = []
-                            for month in host_data:
-                                mid = month["id"]
-                                if mid > cutoff_date and mid != "total":
-                                    uptime_val = month["uptime"]
-                                    if uptime_val != "-":
-                                        host_stats["uptime_monthly"][mid] = uptime_val
-                                        uptimes.append(uptime_val)
-                            host_stats["uptime_average"] = (
-                                functools.reduce(lambda x, y: x + y, uptimes) / float(len(uptimes))
-                                if uptimes
-                                else 100.0
-                            )
-                    # Last week's stats per host
-                    weekly_url = config.reporting.uptime["results_url"].format(uuid=uuid)
-                    async with hc.get(weekly_url, params={"format": "json", "span": 168, "limit": 20000}) as wreq:
-                        if wreq.status == 200:
-                            weekly_json = await wreq.json()
-                            checks_done = len(weekly_json)
-                            checks_failed = len([x for x in weekly_json if x["su"] is False])
-                            weekly_uptime = 100.0
-                            if checks_done:
-                                weekly_uptime -= float((checks_failed / checks_done) * 100)
-                            host_stats["uptime_past_week"] = weekly_uptime
-                    tmpstats[uuid] = host_stats
+        try:
+            async with hc.get(nodeping_summary_url) as req:
+                if req.status == 200:
+                    hosts_json = await req.json()
+                    tmpstats = {}
+                    for host_entry in hosts_json.values():
+                        uuid = host_entry["uuid"]
+                        host_stats = {
+                            "uuid": uuid,
+                            "label": host_entry["label"],
+                            "uptime_monthly": {},
+                            "uptime_average": 100.0,
+                            "uptime_past_week": 100.0,
+                        }
+                        # Monthly stats per host
+                        host_url = config.reporting.uptime["host_url"].format(uuid=uuid)
+                        async with hc.get(host_url, params={"format": "json"}) as hreq:
+                            if req.status == 200:
+                                host_data = await hreq.json()
+                                uptimes = []
+                                for month in host_data:
+                                    mid = month["id"]
+                                    if mid > cutoff_date and mid != "total":
+                                        uptime_val = month["uptime"]
+                                        if uptime_val != "-":
+                                            host_stats["uptime_monthly"][mid] = uptime_val
+                                            uptimes.append(uptime_val)
+                                host_stats["uptime_average"] = (
+                                    functools.reduce(lambda x, y: x + y, uptimes) / float(len(uptimes))
+                                    if uptimes
+                                    else 100.0
+                                )
+                        # Last week's stats per host
+                        weekly_url = config.reporting.uptime["results_url"].format(uuid=uuid)
+                        async with hc.get(weekly_url, params={"format": "json", "span": 168, "limit": 20000}) as wreq:
+                            if wreq.status == 200:
+                                weekly_json = await wreq.json()
+                                checks_done = len(weekly_json)
+                                checks_failed = len([x for x in weekly_json if x["su"] is False])
+                                weekly_uptime = 100.0
+                                if checks_done:
+                                    weekly_uptime -= float((checks_failed / checks_done) * 100)
+                                host_stats["uptime_past_week"] = weekly_uptime
+                        tmpstats[uuid] = host_stats
+                        _stats.update(tmpstats)
+                    _stats.clear()
                     _stats.update(tmpstats)
-                _stats.clear()
-                _stats.update(tmpstats)
-
+        except aiohttp.client_exceptions.ClientError as e:  # Request went awry??
+            print(f"Connection to {nodeping_summary_url} failed: {e}")
+            print("Retrying later..")
 
 async def scan_loop():
     while True:
