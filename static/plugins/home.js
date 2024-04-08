5966ece6118b93ec25188eff84e90bf988015e97
Daniel Gruno <humbedooh@apache.org>
2024-04-08 19:55:27 +0200

    NR: Add users@infra email address as contact

--- static/plugins/home.js
+++ static/plugins/home.js
@@ -4,7 +4,7 @@ async function render_home() {
     const page_description = document.getElementById('page_description');
     page_description.innerText = "This site contains a collection of reports on the overall health and activity of the infrastructure at the ASF. \
     Some reports are open to the public, while others are restricted to those that genuinely need them. If you cannot access a report, chances are \
-    you are not supposed to. Use the menu on the left hand side to pick a report to display.";
+    you are not supposed to. Use the menu on the left hand side to pick a report to display. If you have questions, please reach out to users@infra.apache.org";
 
     const chart_area = document.getElementById('chart_area');
     chart_area.innerText = '';
