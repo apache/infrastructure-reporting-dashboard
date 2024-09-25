c5fb1a816c71b287be8766fa806c529143786547
Drew <dfoulks@apache.org>
2024-09-25 13:37:16 -0400

    Added aliases and updated committees script, now generating almost the whole committees-info object

--- server/app/plugins/committees.py
+++ server/app/plugins/committees.py
@@ -36,22 +36,25 @@ _committers: dict = {}
 
 NAME_MAP = None
 COMMITTEES = None
+COMMITTEE_INFO_JSON = None
 
+# JSON get committee_info.json
+def get_committee_info():
+    return globals()['COMMITTEE_INFO_JSON']
+    
+def get_different_info():
+    return True
+
+# Needs to be async
 def load_data():
     creds = get_creds()
     client = svn.remote.RemoteClient('https://svn.apache.org/repos/private/committers/board', creds['usernm'], creds['passwd'])
-    globals()['NAME_MAP'] = yaml.safe_load(open('tlp_aliases.yml', 'r'))
+    globals()['NAME_MAP'] = yaml.safe_load(open('aliases.yml', 'r'))
     try:
         globals()['COMMITTEES'] = re.split('\n\d\.', client.cat('committee-info.txt').decode('utf-8'))
     except svn.exception.SvnException as e:
-        print("There was an error in the SVN call, please ensure your credentials are correct")
+        sys.stderr.write("There was an error in the SVN call, please ensure your credentials are correct")
         sys.exit(0)
-
-def get_creds():
-    return { "usernm": input("Username: "), "passwd": getpass.getpass("Password: ") }
-
-def get_committers():
-    return _committers
 
 class Committee:
     def __init__(self):
@@ -62,73 +65,118 @@ class Committee:
         self.report_sched = []
         self.display_name = None
 
-#def parse_officers(officers, committees):
+# Needs to go away and use system creds instead
+def get_creds():
+    sys.stderr.write("Username: ")
+    return { "usernm": input(), "passwd": getpass.getpass("Password: ") }
+
+def set_key(project):
+    if project.lower() in globals()['NAME_MAP'].keys():
+        key = globals()['NAME_MAP'][project.lower()]
+    else:
+        key = project.lower()
+
+    return key
 
 def parse_rosters(data):
     projects = re.split('\n\* ', data)
     projects_processed = 0
+    boards_processed = 0
     committees = {}
+    pmcs = {}
     roster_counts = {}
-    for project in projects[1:-1]:
+    for project in projects[1:]:
         proj = Committee()
         roster = {}
         key = None
+        ctype = None
         p = project.split('\n')
         for l in p:
             if l and re.match(r"^[A-z]", l):
                 t = [ item.strip(')').strip() for item in l.split(' (') ]
+                ctype = t[-1].lstrip('(')
                 setattr(proj, "display_name", t[0].capitalize())
                 setattr(proj, "established", t[1])
-                if t[0].lower() in NAME_MAP:
-                    key = NAME_MAP[t[0].lower()]
-                else:
-                    key = t[0].lower()
+                key = set_key(t[0])
             if l and re.match(r"^\s{4}", l):
                 u = re.split(r'\s{2,}', l.strip('^    '))
                 if len(u) > 2: roster[u[0]] = {'name': f"{u[1]}", 'date': f"{u[2]}"}
             setattr(proj, "roster", roster)
             setattr(proj, "roster_count", len(roster))
+        if ctype == "President's Committee" or ctype == "Board Committee":
+            committees[key] = vars(proj)
+            boards_processed += 1
+        else:
             roster_counts[key] = len(roster.keys())
-        committees[key] = vars(proj)
-        projects_processed += 1
-    return projects_processed, committees, roster_counts
+            pmcs[key] = vars(proj)
+            projects_processed += 1
+    return projects_processed, pmcs, roster_counts, committees, boards_processed
+
 
 def parse_committees_info():
     """builds committee-info.json"""
-    projects_processed, committees, roster_counts = parse_rosters(COMMITTEES[3])
+    projects_processed, pmcs, roster_counts, committees, boards_processed = parse_rosters(COMMITTEES[3])
     chairs, boards, prescoms, execs, officers = re.split(r":\n\n", COMMITTEES[1])[1:]
+    pmcs.update(committees)
+    # Parse Chairs
     for chair in chairs.split('\n')[2:-3]:
-        d = re.split("\s\s+", chair.lstrip())
-        if d[0].lower() in NAME_MAP.keys():
-            key = NAME_MAP[d[0].lower()]
-        elif re.match(r"portable runtime", d[0].lower()):
-            key = "apr"
-        elif re.match(r"zookeeper", d[0].lower()):
-            key = "zookeeper"
-        else:
-            key = d[0].lower()
-        
-        committees[key]["chair"] = {} 
-        committees[key]["chair"][d[1].split()[0]] = {"name": d[1].split()[1].split("@")[0]}
-    print(committees)
+        c = re.split("\s\s+", chair.lstrip())
+        key = set_key(c[0])
 
+        # weird edge cases here:
+        if re.match(r"portable runtime", c[0].lower()):
+            key = "apr"
+
+        if key in pmcs.keys():        
+            pmcs[key]["chair"] = {} 
+            pmcs[key]["chair"][c[1].split()[0]] = {"name": c[1].split()[1].split("@")[0]}
+        else:
+            continue
+    officers_temp = {}
+
+    # Parse Boards
+    for board in boards.split('\n')[2:-3]:
+        b = re.split('\s\s+', board.lstrip())
+        temp_roster = b[1].strip('>').split(' <')
+        temp_roster.append(temp_roster[1].split('@')[0])
+#        officers_temp[set_key(b[0])] = { "display_name": b[0],"paragraph": "Committee", "roster": { f"{temp_roster[2]}": { "name": temp_roster[0]}}}
+
+    # parse Presidential Committees
+    for prescom in prescoms.split('\n')[2:-3]:
+        p = re.split('\s\s+', prescom.lstrip())
+
+    # parse Executive Officers
+    for executive in execs.split('\n')[2:-3]:
+        x = re.split('\s\s+', executive.lstrip())
+        temp_roster = x[1].strip('>').split(' <')
+        temp_roster.append(temp_roster[1].split('@')[0])
+        officers_temp[set_key(x[0])] = { "display_name": x[0],"paragraph": "Executive Officer", "roster": { f"{temp_roster[2]}": { "name": temp_roster[0]}}}
+
+    # parse Additional Officers
+    for officer in officers.split('\n')[2:-9]:
+        o = re.split('\s\s+', officer.lstrip())
+        temp_roster = o[1].strip('>').split(' <')
+        temp_roster.append(temp_roster[1].split('@')[0])
+        officers_temp[set_key(o[0])] = { "display_name": o[0],"paragraph": "Additional Officers", "roster": { f"{temp_roster[2]}": { "name": temp_roster[0]}}}
+    
     data = { 
         "last_updated": str(datetime.datetime.now().isoformat()),
         "roster_counts": roster_counts,
-        "committees": committees,
-        "committee_count": projects_processed,
+        "committees": pmcs,
+        "committee_count": projects_processed + boards_processed,
+        "pmc_count": projects_processed,
+        "officers": officers_temp
     }
-    return data
-#    _committers.clear()
+    # Update Global
+    globals()['COMMITTEE_INFO'] = data
+
 
 async def scan_loop():
     while True:
-        await mail_scan()
+        await parse_committees_info()
         await asyncio.sleep(300)
 
-committees = {}
 load_data()
-data = parse_committees_info()
-#print(json.dumps(data, indent=4))
-#committees.update(parse_rosters(COMMITTEES[3]))
-#plugins.root.register(scan_loop, slug="mailstats", title="Mail Transport Statistics", icon="bi-envelope-exclamation-fill", private=True)
+parse_committees_info()
+print(COMMITTEE_INFO)
+#plugins.root.register(scan_loop, slug="json", title="Mail Transport Statistics", icon="bi-envelope-exclamation-fill", private=True)
